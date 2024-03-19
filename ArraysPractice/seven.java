88e339ce09255018be7ebf771259a5c577580eae
Binod <bidhaksubedi77@gmail.com>
2024-03-19 21:12:48 +0530

    optimized code

--- ArraysPractice/seven.java
+++ ArraysPractice/seven.java
@@ -10,32 +10,24 @@ package ArraysPractice;
 public class seven {
     public static int maxProfit(int[] prices) {
         int len = prices.length;
-        if (len == 0)
-            return 0; // No profit if prices array is empty
+        if (len < 2)
+            return 0; // Cannot make a profit with less than 2 prices
+        
+        int minPrice = prices[0];
+        int maxProfit = 0;
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        int min_index = 0;
-        int max_index = 0;
-
-        // Finding the minimum price and its index
-        for (int i = 0; i < len; i++) {
-            if (prices[i] < min) {
-                min = prices[i];
-                min_index = i;
+        for (int i = 1; i < len; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int currentProfit = prices[i] - minPrice;
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit;
+                }
             }
         }
 
-        // Finding the maximum price and its index after the minimum price index
-        for (int i = min_index; i < len; i++) {
-            if (prices[i] > max) {
-                max = prices[i];
-                max_index = i;
-            }
-        }
-
-        int profit = prices[max_index] - prices[min_index];
-        return profit;
+        return maxProfit;
     }
     public static void main(String[]args){
         int profit =maxProfit(new int[]{7, 1, 5, 3, 6, 4});
